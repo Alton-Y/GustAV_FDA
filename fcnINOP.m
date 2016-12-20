9ede07dbdf5d5b7ec0e26e78b6dc3c7b6efdb9da
Alton-Y <alton.yeung@ryerson.ca>
2016-12-20 01:23:13 -0500

    add fcnINOP

---
+++ fcnINOP.m
@@ -0,0 +1,12 @@
+function [ INOP ] = fcnINOP( FMT )
+%FCNINOP Summary of this function goes here
+%   Detailed explanation goes here
+
+INOP = struct;
+% Check ARSP INOP
+if max(FMT.ARSP.Airspeed) < 0.001
+    INOP.ARSP = true;
+end
+
+end
+
